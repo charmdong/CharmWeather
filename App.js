1f23e46f2928a5e557bc537faf635165a852ac4b
Donggun.Chung <chungdk1117@gmail.com>
2022-10-16 22:08:52 +0900

    :tada: 2022.10.16.Sun [Make Weatehr App with React Native]

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Charm Weather! 동기화가 된다고?</Text>
       <StatusBar style="auto" />
     </View>
   );

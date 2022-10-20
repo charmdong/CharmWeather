2c7a5b3c3af7cae864642e8ce55b453bc31d67cb
Donggun.Chung <chungdk1117@gmail.com>
2022-10-20 20:42:10 +0900

    :hammer: 2022.10.20.Thu [5일 3시간]

--- App.js
+++ App.js
@@ -2,15 +2,26 @@ import React from "react";
 import { View, Text, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from "react-native";
 import { useEffect, useState } from "react/cjs/react.development";
 import * as Location from 'expo-location';
+import { Fontisto } from '@expo/vector-icons';
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
-const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+const API_KEY = "6de25efd916beedd583a9be522a0e02d";
+const icons = {
+  Clear: "day-sunny",
+  Clouds: "cloudy",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snowflake",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 function App(props) {
 
   const [city ,setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
+
   const getWeather = async () => {
     // 사용자 위치 정보
     const {granted} = await Location.requestForegroundPermissionsAsync();
@@ -22,12 +33,17 @@ function App(props) {
     const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
     setCity(location[0].city);
     
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&unit=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     const json = await response.json();
     console.log(json);
-    setDays(json.daily);
-    
+    setDays(json.list);
   }; 
+
+  const getDate = (dt) => {
+    let result = dt.replaceAll('-', '/').substr(0, 13);
+    result += "h";
+    return result;
+  }
 
   useEffect(() => {
     getWeather();
@@ -44,7 +60,7 @@ function App(props) {
         pagingEnabled
         showsHorizontalScrollIndicator={false}
       >
-        {days?.length === 0 ? 
+        {days.length === 0 ? 
           (
             <View style={styles.day}>
               <ActivityIndicator color="white" size="large" style={{
@@ -56,7 +72,9 @@ function App(props) {
           (
             days.map((day, index) => 
               <View key={index} style={styles.day}>
-                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={108} color="black" />
+                <Text style={styles.date}>{getDate(day.dt_txt)}</Text>
+                <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}C°</Text>
                 <Text style={styles.description}>{day.weather[0].main}</Text>
                 <Text style={styles.tinyText}>{day.weather[0].description}</Text>
               </View>
@@ -69,26 +87,47 @@ function App(props) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "yellow" },
-  city: { flex: 1.2, justifyContent: "center", alignItems: "center" },
-  cityName: {fontSize: 68, fontWeight: "500"}, 
+  container: { 
+    flex: 1, 
+    backgroundColor: "tomato" 
+  },
+  city: { 
+    flex: 1.2, 
+    justifyContent: "center", 
+    alignItems: "center",
+    marginTop: 30,
+  },
+  cityName: {
+    fontSize: 68, 
+    fontWeight: "500",
+    color: "white",
+  }, 
   weather: { 
 
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems:"center"
+    alignItems:"center",
+    color: "white",
+  },
+  date: {
+    marginTop: 30,
+    fontSize: 40,
+    color: "white",
   },
   temp: {
-    marginTop: 50,
-    fontSize: 178,
+    marginTop: 30,
+    fontSize: 88,
+    color: "white",
   },
   description: {
-    marginTop:-30,
+    marginTop:30,
     fontSize: 60,
+    color: "white",
   },
   tinyText: {
-    fontSize: 20
+    fontSize: 20,
+    color: "white",
   }
 });
 
